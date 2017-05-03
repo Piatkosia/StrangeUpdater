3cfd0f96048e12ee47fe1adb459830dc2c8be517
piatk <piatk@A-KUKU-2>
2017-05-03 19:54:50 +0200

    Create tests for version parser

--- UpdaterTests/Tests.cs
+++ UpdaterTests/Tests.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using StrangeUpdater;
+using Xunit;
 namespace UpdaterTests
 {
     public class Tests
     {
-
+        [Fact]
+        public void VersionParserForFileWorks()
+        {
+            VersionParser parser = new VersionParser();
+            Assert.Equal(parser.GetVersionFromDisc(@"C:\Users\piatk\Desktop\VERSION.txt"), 2);
+        }
+        [Fact]
+        public void VersionParserForNetworkWorks()
+        {
+            VersionParser parser = new VersionParser();
+            Assert.Equal(parser.GetVersionFromNet(@"http://77.81.226.38/patcher/VERSION"), 2);
+        }
     }
 }
